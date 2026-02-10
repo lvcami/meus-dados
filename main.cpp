a96f7bd95aafa8d719a1239d72b4c3dfc9e7e558
lvcami <163419225+lvcami@users.noreply.github.com>
2026-02-09 22:51:56 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    //variaveis inteiras
+    int idade = 17;
+    int ano = 2026;
+
+    //variaveis reais
+    float altura = 1.63;
+
+    //variavel booleana 
+    bool aprovado = true;
+
+    //variavel texto
+    string nome = "Camila Lopes";
+
+    return 0;
+}
